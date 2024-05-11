ffea1c8169f80d60bca338749c175f07b2bc2c84
关梦园 <guan.mengyuan@icloud.com>
2024-05-11 10:01:23 +0800

    add auto import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import {defineConfig} from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
     const UnoCSS = (await import("unocss/vite")).default;
     return {
-        plugins: [uni(), UnoCSS()],
+        plugins: [
+            AutoImport({
+                imports: [
+                    'vue',
+                    'uni-app',
+                ]
+            })
+            , uni(), UnoCSS()],
     }
 });
