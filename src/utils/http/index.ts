9d175c1e4db8bdb869b692c3b8617419b934050d
关梦园 <guan.meng.yuan@icloud.com>
2025-02-25 00:55:55 +0800

    修复支付宝可能编译失败的问题

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -1,74 +1,72 @@
-import { isH5 } from '@uni-helper/uni-env'
-
 class defHttp {
-  private static async callApi<T>(requestOption: RequestOption) {
-    const httpUrl = isH5 ? import.meta.env.VITE_API_PREFIX : import.meta.env.VITE_HTTP_URL
-    const info = uni.getSystemInfoSync()
-    const ClientType = info.uniPlatform
-    return new Promise<T>((resolve, reject) => {
-      if (requestOption.needLoading) {
-        requestOption.toast?.loading(requestOption.loadingMessage || '加载中')
-      }
-      uni.request({
-        url: `${httpUrl}/${requestOption.url}`,
-        method: requestOption.method,
-        data: requestOption.data,
-        header: {
-          Authorization: uni.getStorageSync('TOKEN') || '',
-          ClientType,
-        },
-        success: (res) => {
-          let data = res.data as Res<T>
-          if (res.statusCode !== 200 || !data || !data.success) {
+    static async get<T>(requestOption: RequestOption) {
+        requestOption.method = 'GET'
+        return defHttp.callApi<T>(requestOption)
+    }
+
+    static async post<T>(requestOption: RequestOption) {
+        requestOption.method = 'POST'
+        return defHttp.callApi<T>(requestOption)
+    }
+
+    static async delete<T>(requestOption: RequestOption) {
+        requestOption.method = 'DELETE'
+        return defHttp.callApi<T>(requestOption)
+    }
+
+    static async put<T>(requestOption: RequestOption) {
+        requestOption.method = 'PUT'
+        return defHttp.callApi<T>(requestOption)
+    }
+
+    private static async callApi<T>(requestOption: RequestOption) {
+        const httpUrl = uni.getSystemInfoSync().uniPlatform === 'web' ? import.meta.env.VITE_API_PREFIX : import.meta.env.VITE_HTTP_URL
+        const info = uni.getSystemInfoSync()
+        const ClientType = info.uniPlatform
+        return new Promise<T>((resolve, reject) => {
             if (requestOption.needLoading) {
-              requestOption.toast?.close()
+                requestOption.toast?.loading(requestOption.loadingMessage || '加载中')
             }
-            requestOption.toast?.error({
-              msg: data.tips || '网络异常',
+            uni.request({
+                url: `${httpUrl}/${requestOption.url}`,
+                method: requestOption.method,
+                data: requestOption.data,
+                header: {
+                    Authorization: uni.getStorageSync('TOKEN') || '',
+                    ClientType,
+                },
+                success: (res) => {
+                    let data = res.data as Res<T>
+                    if (res.statusCode !== 200 || !data || !data.success) {
+                        if (requestOption.needLoading) {
+                            requestOption.toast?.close()
+                        }
+                        requestOption.toast?.error({
+                            msg: data.tips || '网络异常',
+                        })
+                        return
+                    }
+                    if (typeof data === 'string') {
+                        data = JSON.parse(data)
+                    }
+
+                    if (requestOption.needLoading) {
+                        requestOption.toast?.close()
+                    }
+                    resolve(data.data as T)
+                },
+                fail: (error) => {
+                    if (requestOption.needLoading) {
+                        requestOption.toast?.close()
+                    }
+                    requestOption.toast?.error({
+                        msg: '网络异常',
+                    })
+                    reject(error)
+                },
             })
-            return
-          }
-          if (typeof data === 'string') {
-            data = JSON.parse(data)
-          }
-
-          if (requestOption.needLoading) {
-            requestOption.toast?.close()
-          }
-          resolve(data.data as T)
-        },
-        fail: (error) => {
-          if (requestOption.needLoading) {
-            requestOption.toast?.close()
-          }
-          requestOption.toast?.error({
-            msg: '网络异常',
-          })
-          reject(error)
-        },
-      })
-    })
-  }
-
-  static async get<T>(requestOption: RequestOption) {
-    requestOption.method = 'GET'
-    return defHttp.callApi<T>(requestOption)
-  }
-
-  static async post<T>(requestOption: RequestOption) {
-    requestOption.method = 'POST'
-    return defHttp.callApi<T>(requestOption)
-  }
-
-  static async delete<T>(requestOption: RequestOption) {
-    requestOption.method = 'DELETE'
-    return defHttp.callApi<T>(requestOption)
-  }
-
-  static async put<T>(requestOption: RequestOption) {
-    requestOption.method = 'PUT'
-    return defHttp.callApi<T>(requestOption)
-  }
+        })
+    }
 }
 
-export { defHttp }
+export {defHttp}
--- uno.config.ts
+++ uno.config.ts
@@ -1,37 +1,36 @@
-import type { Preset, SourceCodeTransformer } from 'unocss'
-import { defineConfig, presetAttributify, presetIcons, presetTypography, presetUno } from 'unocss'
-import { presetApplet, presetRemRpx, transformerAttributify } from 'unocss-applet'
+import {defineConfig, presetAttributify, presetIcons, presetTypography, presetUno} from 'unocss'
+import {presetApplet, presetRemRpx, transformerAttributify} from 'unocss-applet'
+
 // uni-app
-const isApplet = process.env?.UNI_PLATFORM?.startsWith('mp-') ?? false
+const isApplet = process.env.UNI_PLATFORM?.startsWith('mp-') ?? false
 // taro
 // const isApplet = process.env.TARO_ENV !== 'h5' ?? false
-const presets: Preset[] = []
-const transformers: SourceCodeTransformer[] = []
+const presets: any[] = []
+const transformers: any[] = []
 
 if (isApplet) {
-  presets.push(presetApplet())
-  presets.push(presetRemRpx())
-  transformers.push(transformerAttributify({ ignoreAttributes: ['block'] }))
-}
-else {
-  presets.push(presetApplet())
-  presets.push(presetAttributify())
-  presets.push(presetRemRpx({ mode: 'rpx2rem' }))
+    presets.push(presetApplet())
+    presets.push(presetRemRpx())
+    transformers.push(transformerAttributify({ignoreAttributes: ['block']}))
+} else {
+    presets.push(presetApplet())
+    presets.push(presetAttributify())
+    presets.push(presetRemRpx({mode: 'rpx2rem'}))
 }
 export default defineConfig({
-  presets: [
-    presetUno(),
-    presetIcons(),
-    presetTypography(),
-    ...presets,
-  ],
-  transformers: [
-    // ...
-    ...transformers,
-  ],
-  shortcuts: [{
-    'c-p': 'c-#4d80f0',
-    'c-tip': 'c-#bfbfbf',
-    'h-f': 'h-94.8459vh',
-  }],
+    presets: [
+        presetUno(),
+        presetIcons(),
+        presetTypography(),
+        ...presets,
+    ],
+    transformers: [
+        // ...
+        ...transformers,
+    ],
+    shortcuts: [{
+        'c-p': 'c-#4d80f0',
+        'c-tip': 'c-#bfbfbf',
+        'h-f': 'h-94.8459vh',
+    }],
 })
